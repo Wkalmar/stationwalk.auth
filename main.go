aa8b1540e326344e05b14eaa9c6e3a36e0a5d712
Bohdan Stupak <stupakbogdan@ukr.net>
2019-09-27 17:16:57 +0300

    Store password as a sequence of bytes

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 
 const login = "su"
 const salt = "<your salt>"
-const password = "<your password here>"
 
 type Credentials struct {
 	Login    string `json:"login"`
@@ -30,11 +29,13 @@ func areSlicesEqual(a []byte, b []byte) bool {
 }
 
 func HandleRequest(ctx context.Context, credentials Credentials) (string, error) {
+	password := []byte{221, 35, 76, 136, 29, 114, 39, 75, 41, 248, 62, 216, 149, 39, 248, 154, 243, 203, 188, 106, 206, 74, 122, 47, 255, 61, 173, 43, 102, 173, 222, 125}
+
 	if credentials.Login != login {
 		return "auth failed", errors.New("auth failed")
 	}
 	key := argon2.Key([]byte(credentials.Password), []byte(salt), 3, 128, 1, 32)
-	if areSlicesEqual(key, []byte(password)) {
+	if areSlicesEqual(key, password) {
 		return "ok", nil
 	}
 	return "auth failed", errors.New("auth failed")
